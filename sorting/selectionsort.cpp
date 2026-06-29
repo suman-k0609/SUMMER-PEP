f4a363f41602e494385ccf76192ce827fd897855
Suman Kumari <sumankumari200603@gmail.com>
2026-06-29 11:35:43 +0530

    save

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -77,3 +77,34 @@ int main() {
 
     return 0;
 }
+
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int arr[]={6,5,4,8,2,1};
+  int n=6;
+        for(int i=n-1;i>=0;i--){
+            int mini=i;
+        for(int j=n-1;j>=i+1;j--){
+            if(arr[j]>arr[mini]){
+                mini=j;
+            }
+            
+            
+        }
+        swap(arr[i],arr[mini]);
+         for(int k=0;k<n;k++){
+            cout<<arr[k]<<" ";
+        }
+        
+       
+        cout<<endl;
+    }
+
+
+    return 0;
+}
