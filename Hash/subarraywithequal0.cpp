83b75afff5c6b25d24b83491243a0ca73f93692e
Suman Kumari <sumankumari200603@gmail.com>
2026-07-02 10:36:10 +0530

    save

---
+++ Hash/subarraywithequal0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int countSubarray(vector<int>& arr) {
+        // code here
+        unordered_map<int,int> mp;
+        int prefix=0;
+        mp[0]=1;
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
+          if (arr[i] == 0)
+                arr[i] = -1;
+
+            prefix += arr[i];
+
+            if (mp.find(prefix) != mp.end())
+                ans += mp[prefix];
+
+            mp[prefix]++;
+        }
+
+        return ans;
+      
+        
+    }
+};
